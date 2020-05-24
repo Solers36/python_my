e1dd93a76fb9708b4e8b39c7779d9a8483c6f549
Solers36 <mtestov@rambler.ru>
2020-05-24 21:38:12 +0300

    Выполнил задание 3

--- chapter_3/generate_usernames_testov.py
+++ chapter_3/generate_usernames_testov.py
@@ -1,15 +1,15 @@
-"""Модифицируйте программу generate_usernames.py так, чтобы в 
-каждой строке она выводила информацию о двух пользователях, 
-ограничив длину имени 17 символами; через каждые 64 строки 
-программа должна выводить символ перевода формата и в начале 
-каждой страницы она должна выводить заголовки столбцов. 
+"""Модифицируйте программу generate_usernames.py так, чтобы в
+каждой строке она выводила информацию о двух пользователях,
+ограничив длину имени 17 символами; через каждые 64 строки
+программа должна выводить символ перевода формата и в начале
+каждой страницы она должна выводить заголовки столбцов.
 Это достаточно сложно. Вам потребуется сохранить заголовки
 столбцов в переменных, чтобы потом их можно было использовать
 по мере необходимости, и изменить спецификаторы формата, что бы
-обеспечить вывод более коротких имен. Один из способов обеспечить 
+обеспечить вывод более коротких имен. Один из способов обеспечить
 постраничный вывод заключается в том, чтобы сохранить все
 выводимые строки в списке, а затем выполнить обход списка, используя
-оператор извлечения среза с шагом для получения элементов 
+оператор извлечения среза с шагом для получения элементов
 слева и справа и применяя функцию zip() для их объединения.
 Решение приводится в файле generate_usernames_ans.py, а достаточно
 большой объем исходных данных вы найдете в файле data/users2.txt."""
@@ -21,7 +21,7 @@ import sys
 ID, FORENAME, MIDDLENAME, SURNAME, DEPARTMENT = range(5)
 
 User = collections.namedtuple("User",
-            "username forename middlename surname id")
+                              "username forename middlename surname id")
 
 
 def main():
@@ -39,7 +39,7 @@ def main():
                 if line:
                     user = process_line(line, usernames)
                     users[(user.surname.lower(), user.forename.lower(),
-                            user.id)] = user
+                           user.id)] = user
     print_users(users)
 
 
@@ -67,10 +67,12 @@ def print_users(users):
     namewidth = 17
     usernamewidth = 9
 
-    print("{0:<{nw}} {1:^6} {2:{uw}}".format(
-          "Name", "ID", "Username", nw=namewidth, uw=usernamewidth))
-    print("{0:-<{nw}} {0:-<6} {0:-<{uw}}".format(
-          "", nw=namewidth, uw=usernamewidth))
+    header = "{0:<{nw}} {1:^6} {2:{uw}} ".format(
+        "Name", "ID", "Username", nw=namewidth, uw=usernamewidth)
+    separator = "{0:-<{nw}} {0:-<6} {0:-<{uw}} ".format(
+        "", nw=namewidth, uw=usernamewidth)
+
+    users_to_print = []
 
     for key in sorted(users):
         user = users[key]
@@ -78,9 +80,23 @@ def print_users(users):
         if user.middlename:
             initial = " " + user.middlename[0]
         name = "{0.surname}, {0.forename}{1}".format(user, initial)
-        name = name[:17]
-        print("{0:.<{nw}} ({1.id:4}) {1.username:{uw}}".format(
-              name, user, nw=namewidth, uw=usernamewidth))
+        users_to_print.append("{0:.<{nw}.{nw}} ({1.id:4}) {1.username:{uw}}".format(
+            name, user, nw=namewidth, uw=usernamewidth))
+
+   
+    line_number = 0
+    lines_per_page = 64
+    for first_column, second_column in zip(users_to_print[0::2], users_to_print[1::2]):
+        if line_number == 0:
+            print("{0} {0}\n{1} {1}".format(header, separator))
+        print("{0}  {1}".format(first_column, second_column))
+        line_number += 1
+        if line_number == lines_per_page:
+            print("\n")
+            line_number = 0
+    if users_to_print[-1] != second_column:
+        print(users_to_print[-1])
+
 
 
 main()
