d68e3dd4cd45d0e65837e3cd78aea0a2225740de
Solers36 <mtestov@rambler.ru>
2020-07-02 23:56:03 +0300

    Выполнил задание 10 главы сайта.

---
+++ website/Chapter_10_Local_and_global_variables_testov.py
@@ -0,0 +1,40 @@
+"""В основной ветке программы вызывается функция cylinder(), которая вычисляет площадь цилиндра. 
+В теле cylinder() определена функция circle(), вычисляющая площадь круга по формуле πr2. 
+В теле cylinder() у пользователя спрашивается, хочет ли он получить только площадь боковой 
+поверхности цилиндра, которая вычисляется по формуле 2πrh, или полную площадь цилиндра. 
+В последнем случае к площади боковой поверхности цилиндра должен добавляться удвоенный результат 
+вычислений функции circle()."""
+
+import math
+
+
+def cylinder():
+    def circle():
+        area_circle = 2 * math.pi * radius
+        return area_circle
+    while True:
+        try:
+            radius = float(input("Радиус: "))
+            height = float(input("Высота: "))
+            break
+        except ValueError:
+            print("Используйте цифры.")
+
+    while True:
+        try:
+            number = int(input(
+                "Получить площадь боковой поверхности цилиндра - 1, полную площадь цилиндра - 2: "))
+            if number == 1:
+                area = 2 * math.pi * radius * height
+                break
+            elif number == 2:
+                area = (2 * math.pi * radius * height) + 2 * circle()
+                break
+            else:
+                raise ValueError()
+        except ValueError:
+            print("Используйте цифры 1 или 2.")
+    print("Площадь: %.2f" % area)
+
+
+cylinder()
