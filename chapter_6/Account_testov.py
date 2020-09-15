3b424386c538ec01c9789719c491f9504c9eff43
Solers36 <mtestov@rambler.ru>
2020-09-15 11:59:55 +0300

    Выполнил 3 задание из 6 главы книги.

--- chapter_6/Account_testov.py
+++ chapter_6/Account_testov.py
@@ -8,3 +8,46 @@ usd_conversion_rate, description и usd (вычисляется, как amount *
 usd_conversion_rate). Реализацию класса можно уместить в шестьде>
 сят строк программного кода, включая несколько простейших доктестов.
 """
+
+
+class Transaction:
+    def __init__(self, amount, date, currency="USD", usd_conversion_rate=1, description=None):
+        """
+        >>> transaction_usd = Transaction(10, 10.10.2010)
+        >>> print(transaction_usd.amount, transaction_usd.currency, transaction_usd.date,
+                    transaction_usd.usd_conversion_rate, transaction_usd.description, transaction_usd.usd)
+        10 USD 10.10.2010 1 None 10    
+        >>> transaction_rub = Transaction(20, "20.02.2020", "RUB", 0.013, "Transaction #123")
+        >>> print(transaction_rub.amount, transaction_rub.currency, transaction_rub.date,
+                    transaction_rub.usd_conversion_rate, transaction_rub.description, transaction_rub.usd)
+        20 RUB 20.02.2020 0.013 Transaction #123 0.26
+        """
+        self.__amount = amount
+        self.__date = date
+        self.__currency = currency
+        self.__usd_conversion_rate = usd_conversion_rate
+        self.__description = description
+
+    @property
+    def amount(self):
+        return self.__amount
+
+    @property
+    def date(self):
+        return self.__date
+
+    @property
+    def currency(self):
+        return self.__currency
+
+    @property
+    def usd_conversion_rate(self):
+        return self.__usd_conversion_rate
+
+    @property
+    def description(self):
+        return self.__description
+
+    @property
+    def usd(self):
+        return self.__amount * self.__usd_conversion_rate
