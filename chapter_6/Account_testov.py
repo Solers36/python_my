186f52840bf498b1d22259bfc5a48aced28b37b3
Solers36 <mtestov@rambler.ru>
2020-09-15 22:27:57 +0300

    Добавил необходимые свойства и методы.

--- chapter_6/Account_testov.py
+++ chapter_6/Account_testov.py
@@ -51,3 +51,111 @@ class Transaction:
     @property
     def usd(self):
         return self.__amount * self.__usd_conversion_rate
+
+
+
+"""
+Реализуйте класс Account, который хранил бы номер счета, назва>
+ние счета и список транзакций (объектов класса Transaction). Номер
+счета должен быть реализован в виде свойства, доступного только
+для чтения. Название счета должно быть реализовано в виде свой>
+ства, доступного для чтения и для записи с проверкой длины назва>
+ния, которое должно содержать не менее четырех символов. Класс
+должен поддерживать встроенную функцию len() (возвращая чис>
+ло транзакций) и содержать два вычисляемых свойства, доступных
+только для чтения: balance, возвращающее баланс счета в долларах
+США, и all_usd, возвращающее True, если все транзакции выполня>
+лись в долларах США, или False – в противном случае. Добавьте
+три дополнительных метода: apply() для добавления транзакции,
+save() и load(). Методы save() и load() должны сохранять и загру>
+жать объекты в двоичном формате, в файле, имя которого совпада>
+ет с номером счета и с расширением .acc. Они должны сохранять
+и загружать номер счета, название счета и все транзакции. Реали>
+зацию класса можно уместить в девяносто строк программного ко>
+да вместе с несколькими простейшими доктестами, включающими
+проверку операций сохранения и загрузки с помощью такого про>
+граммного кода, как name = os.path.join(tempfile.gettempdir(), ac!
+count_name), который позволяет получить подходящее имя времен>
+ного файла. Требуется удалить временные файлы по завершении
+доктестов.
+"""
+class LoadError(Exception):
+    pass
+
+class SaveError(Exception):
+    pass
+
+
+import pickle
+
+class Account:
+    def __init__(self, number, name):
+        """
+        """
+        self.__number = number
+        self.__name = name
+        self.__list_of_transactions = []
+
+    @property
+    def number(self):
+        return self.__number
+
+    @property
+    def name(self):  
+        return self.__name
+
+    @name.setter
+    def name(self):
+        assert len(self.__name) >= 4, "The account name must contain at least four characters."
+        return self.__name
+
+    def __len__(self):
+        return len(self.__list_of_transactions)
+
+    @property
+    def balance(self):
+        account_balance = 0
+        for transaction in self.__list_of_transactions:
+            account_balance += transaction.usd()
+        return account_balance
+
+    @property
+    def all_usd(self):
+        for transaction in self.__list_of_transactions:
+            if transaction.currency() != "USD":
+                return False
+            return True
+
+    def apply(self, new_transaction):
+        assert "Transaction" in type(new_transaction),  "You can only add objects of the 'Transactions' class"
+        return self.__list_of_transactions.append(new_transaction)
+
+    
+    def save(self):
+        """Saves the object in binary format with the extension ". acc"
+        """
+        filename = self.__name + ".acc"
+        fh = None
+        try:
+            data = [self.__number, self.__name, self.__list_of_transactions]
+            fh = open(filename, "wb")
+            pickle.dump(data, fh, pickle.HIGHEST_PROTOCOL)
+        except (EnvironmentError, pickle.PicklingError) as err:
+            raise SaveError(str(err))
+        finally:
+            if fh is not None:
+                fh.close()
+
+    def load(self, filename):
+        """Loads object data from a file
+        """
+        fh = None
+        try:
+            fh = open(filename, "rb")
+            data = pickle.load(fh)
+            (self.__number, self.__name, self.__list_of_transactions) = data
+        except (EnvironmentError, pickle.UnpicklingError) as err:
+            raise LoadError(str(err))
+        finally:
+            if fh is not None:
+                fh.close()
